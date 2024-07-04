33844458c829c5356a4c9de41c62ed935cd81524
adailtoncaze <adailtoncaze@gmail.com>
2024-07-04 07:40:43 -0300

    Transformação em PWA

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  
+  pwa: {
+    name: 'Flashcard App',
+    themeColor: '#4DBA87',
+    msTileColor: '#000000',
+    manifestOptions: {
+      background_color: '#ffffff'
+    },
+    workboxOptions: {
+      skipWaiting: true,
+      clientsClaim: true
+    }
+  }
 })
